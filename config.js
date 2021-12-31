864e1dd856ed58caea07809855793a9a9a06bdc9
DivjotRulez <75211419+DivjotRulez@users.noreply.github.com>
2021-12-31 15:09:46 +0000

    Update config.js
    fixed my config

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * https://beta.developer.spotify.com/dashboard
  */
 
-const client_id = 'YOUR_CLIENT_ID';
-const redirect_uri = 'YOUR_CALLBACK_URL';
-const client_secret = 'YOUR_CLIENT_SECRET';
+const client_id = '26d1146e7b24465dbbebbb7c8ce010d5';
+const redirect_uri = 'http://localhost:3000/callback';
+const client_secret = '065778587ade4d55b0039f95c956902f';
